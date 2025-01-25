3470e82fad53c1415750500f0249776c3c5c825f
polomich <michpolonsky@gmail.com>
2025-01-25 17:48:34 -0500

    arranging layout better

--- controls.js
+++ controls.js
@@ -1,10 +1,30 @@
-const bottoms = ["", "images/dress1.png", "", ""];
+const tops = ["", "images/top1.png", "images/top2.png", "images/top3.png"];
+
+var top = 0;
+
+const bottoms = [
+  "",
+  "images/bottom1.png",
+  "images/bottom2.png",
+  "images/bottom3.png",
+];
 
 var bottom = 0;
 
 const accs = ["", "images/acc1.png", "images/acc2.png", "images/acc3.png"];
 
 var acc = 0;
+
+function nexttop() {
+  if (top == tops.length - 1) {
+    top = 0;
+    document.getElementById("thetop").setAttribute("src", tops[top]);
+  } else {
+    top++;
+    document.getElementById("thetop").setAttribute("src", tops[top]);
+  }
+  console.log(top);
+}
 
 function nextbottom() {
   if (bottom == bottoms.length - 1) {
